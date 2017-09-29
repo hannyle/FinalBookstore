fba6578b96a9025e28746c7ece97ee0101bd6d3c
Le Hang <a1603195@myy.haaga-helia.fi>
2017-09-29 13:27:56 +0300

    some changes

--- src/main/java/fi/hh/server/Bookstore/domain/Book.java
+++ src/main/java/fi/hh/server/Bookstore/domain/Book.java
@@ -1,10 +1,13 @@
 package fi.hh.server.Bookstore.domain;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
@@ -19,6 +22,7 @@ public class Book {
 	public int price;
 	
 	@ManyToOne
+    @JsonIgnore
 	@JoinColumn(name="categoryId")
 	
 	public Category category;
--- src/main/java/fi/hh/server/Bookstore/web/BookStoreController.java
+++ src/main/java/fi/hh/server/Bookstore/web/BookStoreController.java
@@ -1,13 +1,17 @@
 package fi.hh.server.Bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.hh.server.Bookstore.domain.Book;
+import fi.haagahelia.course.domain.Student;
 import fi.hh.server.Bookstore.domain.*;
 import fi.hh.server.Bookstore.domain.BookRepository;
 
@@ -19,19 +23,27 @@ public class BookStoreController {
 	@Autowired
 	private CategoryRepository crepository;
 	
+	//Show all books
+	
     @RequestMapping(value="/booklist")
     public String studentList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
-  
+ // RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
     	model.addAttribute("categories", crepository.findAll());
         return "addbook";
     }     
-    
+ 
+	
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Book book){
         repository.save(book);
